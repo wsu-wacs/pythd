7945d480e73aee91ba1270166e86d045c27e1680
Kyle Brown <brown.718@wright.edu>
2020-02-25 12:07:47 -0500

    Add THDResult class

--- pythd/thd.py
+++ pythd/thd.py
@@ -16,6 +16,11 @@ class THD:
         The filter function to use
     cover : pythd.cover.BaseCover
         The covering to use
+    clustering : pythd.clustering.BaseClustering
+        The clustering algorithm to use
+    group_threshold : int
+        The group threshold to use. Any connected components with
+        less data points than this will not be used
     """
     def __init__(self, dataset, filt, cover,
                  clustering=HierarchicalClustering(),
@@ -28,19 +33,20 @@ class THD:
         self.reset()
         
     def reset(self):
-        job = THDJob(self.dataset, self.filter, self.cover,
+        self.root = THDJob(self.dataset, self.filter, self.cover,
                      rids=list(self.dataset.index.values),
                      clustering=self.clustering,
                      group_threshold=self.group_threshold)
-        self.jobs = [job]
-        self.finished = []
+        self.jobs = [self.root]
 
     def run(self):
         while self.jobs:
-            print(self.jobs)
             job = self.jobs.pop()
             job.run()
             self.jobs += job.child_jobs
+    
+    def get_results(self):
+        return self.root.group
 
 class THDJob:
     """
@@ -62,11 +68,14 @@ class THDJob:
         The minimum size of a child group to be further decomposed
     contract_amount : float
         Amount to shrink the cover by if a split isn't observed
+    parent : THDGroup
+        The parent group of this group. None if this is the root node
     """
     def __init__(self, dataset, filt, cover, rids, 
                  clustering=HierarchicalClustering(),
                  group_threshold=100,
-                 contract_amount=0.1):
+                 contract_amount=0.1,
+                 parent=None):
         self.dataset = dataset
         self.filt = filt
         self.cover = copy.deepcopy(cover)
@@ -75,6 +84,7 @@ class THDJob:
         self.clustering = clustering
         self.group_threshold = group_threshold
         self.contract_amount = contract_amount
+        self.parent = parent
         self.reset()
     
     def reset(self):
@@ -93,6 +103,9 @@ class THDJob:
         self.network = self.result.compute_k_skeleton(k=1)
         self.components = self.network.get_connected_components()
         
+        self.group = THDGroup(self.dataset, self.rids, self.network)
+        if self.parent:
+            self.parent.add_child(self.group)
         # row IDs for each group
         for nids in self.components:
             rids = set()
@@ -115,6 +128,103 @@ class THDJob:
                 job = THDJob(self.dataset, self.filt, new_cover, rids=group,
                              clustering=self.clustering,
                              group_threshold=self.group_threshold,
-                             contract_amount=self.contract_amount)
+                             contract_amount=self.contract_amount,
+                             parent=self.group)
                 self.child_jobs.append(job)
         self.is_run = True
+
+class THDGroup:
+    """
+    Class representing a THD group in a completed THD
+    
+    Parameters
+    ----------
+    dataset : numpy.ndarray or pandas.DataFrame
+        The entire dataset that the THD was run on
+    rids : sequence
+        The row ids of the group
+    network : pythd.complex.SimplicialComplex
+        The simplicial complex produced by MAPPER on the group
+    """
+    def __init__(self, dataset, rids, network):
+        self.dataset = dataset
+        self.rids = set(rids)
+        self.num_rows = len(self.rids)
+        self.network = network
+        self.children = []
+        self.depth = 0
+        self.id = 0
+        self.parent_id = 0
+        self.dcs = {0: 1}
+        # Value used for coloring
+        self.value = len(self.rids) / self.dataset.shape[0]
+
+    def __iter__(self):
+        self.iter_stack = [self]
+        return self
+    
+    def __next__(self):
+        if self.iter_stack:
+            res = self.iter_stack.pop()
+            self.iter_stack = res.children + self.iter_stack
+            return res
+        else:
+            raise StopIteration
+    
+    def __str__(self):
+        return "Group {} ({} rows, {} children)".format(self.get_name(), self.num_rows, len(self.children))
+
+    def _normalize_values(self):
+        minval = 1.0
+        maxval = 0.0
+        for group in self:
+            maxval = max(maxval, group.value)
+            minval = min(minval, group.value)
+        print(minval, maxval)
+        for group in self:
+            group.value = (group.value - minval) / (maxval - minval)
+
+    def get_name(self):
+        return "{}.{}.{}".format(self.depth, self.id, self.parent_id)
+        
+    def add_child(self, child):
+        child.depth = self.depth + 1
+        child.parent_id = self.id
+        child.dcs = self.dcs
+        child.id = self.dcs.get(child.depth, 0)
+        self.dcs[child.depth] = child.id + 1
+        
+        self.children.append(child)
+    
+    def as_igraph_graph(self):
+        import igraph
+        pal = igraph.drawing.colors.AdvancedGradientPalette(["red", "orange", "green", "blue"], n=128)
+        
+        g = igraph.Graph()
+        for group in self:
+            g.add_vertex(name=group.get_name(),
+                         num_rows=self.num_rows,
+                         color=pal.get(round(group.value*127.0)))
+
+        for group in self:
+            for child in group.children:
+                g.add_edge(group.get_name(), child.get_name())
+        return g
+    
+    def color_density(self, normalize=False):
+        for group in self:
+            group.value = len(group.rids) / group.dataset.shape[0]
+        if normalize:
+            self._normalize_values()
+
+    def color_by_rids(self, rids, normalize=False):
+        rids = set(rids)
+        for group in self:
+            group.value = len(rids & group.rids) / len(group.rids)
+        if normalize:
+            self._normalize_values()
+
+    def color_network_size(self):
+        for group in self:
+            group.value = len(group.network.get_k_simplices())
+        self._normalize_values()
