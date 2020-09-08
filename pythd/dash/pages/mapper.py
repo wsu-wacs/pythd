eb331ea8c76ce191aaa1c67d4462062fab3dedea
kbrown718 <brown.718@wright.edu>
2020-09-08 16:45:07 -0400

    ability to upload CSV to dashboard

--- pythd/dash/pages/mapper.py
+++ pythd/dash/pages/mapper.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import base64, io
 
 import numpy as np
 import pandas as pd
@@ -11,7 +12,12 @@ import dash_cytoscape as cyto
 from dash.dependencies import Input, Output, State, ClientsideFunction
 
 from ..app import app
-
+from ...filter import IdentityFilter, ComponentFilter, ScikitLearnFilter
+from ...cover import IntervalCover
+from ...clustering import HierarchicalClustering 
+################################################################################
+# Layout
+################################################################################
 def make_filter_params():
     return html.Div(id='filter-params-div', children=[
         html.Div(id='tsne-params-div', children=[
@@ -76,6 +82,22 @@ layout = html.Div([
                       max=100.0,
                       value=15.0),
             html.Hr(),
+            # Clustering settings
+            html.H4('Clustering'),
+            html.Span('Method: '),
+            dcc.Dropdown(id='cluster-method-dropdown',
+                searchable=False,
+                value='single',
+                options=[
+                    {'label': 'Single Linkage', 'value': 'single'},
+                    {'label': 'Complete Linkage', 'value': 'complete'},
+                    {'label': 'Average Linkage', 'value': 'average'},
+                    {'label': 'Weighted', 'value': 'weighted'},
+                    {'label': 'Centroid', 'value': 'centroid'},
+                    {'label': 'Median', 'value': 'median'},
+                    {'label': 'Ward', 'value': 'ward'}
+                ]),
+            html.Hr(),
             # Other settings
             html.Button('Run MAPPER', id='mapper-button', n_clicks=0)
         ]),
@@ -91,13 +113,30 @@ layout = html.Div([
     html.Div(id='bit-bucket-1', style=dict(display='none'))
 ])
 
-def make_filter_settings_div(filter_name):
-    r=[]
-    if filter_name == 'component':
-        r = [html.Span('Component List: '),
-             dcc.Input(id='filter-component-input')]
-    return html.Div(children=r)
+################################################################################
+# Functions
+################################################################################
+def contents_to_dataframe(contents):
+    contents = contents.split(',')
+    content_type = contents[0].split(';')
+    contents = contents[1]
+    contents = base64.b64decode(contents, validate=True)
 
+    if 'zip' in content_type:
+        with io.BytesIO(contents) as f:
+            df = pd.read_csv(f, header=0, index_col=0, compression='zip')
+    else:
+        with io.StringIO(contents.decode('utf-8')) as f:
+            df = pd.read_csv(f, header=0, index_col=0)
+    return df
+
+def get_filter(name, *args):
+    if name == 'tsne':
+        n_components = int(args[0])
+        return ScikitLearnFilter(TSNE, n_components=n_components)
+################################################################################
+# Callbacks
+################################################################################
 app.clientside_callback(
         ClientsideFunction(
             namespace='clientside',
@@ -113,13 +152,34 @@ def on_mapper_upload_change(contents, filename):
     ctx = dash.callback_context
     if not ctx.triggered:
         return dash.no_update
-
-    p = Path(filename)
-    if p.suffix == '.zip':
-        pass # zip file
-    elif p.suffix == '.csv':
-        pass # csv file
-
+    
     return 'Uploaded file: ' + filename
 
+@app.callback(Output('mapper-graph', 'elements'),
+             [Input('mapper-button', 'n_clicks')],
+             [State('mapper-upload', 'contents'),
+              State('filter-dropdown', 'value'),
+              # Cover parameters
+              State('cover-interval-input', 'value'),
+              State('cover-overlap-input', 'value'),
+              # Filter-specific parameters
+              # tSNE parameters
+              State('tsne-components-input', 'value'),
+              # Component filter parameters
+              State('filter-component-input', 'value'),
+])
+def on_run_mapper_click(n_clicks, contents, filter_name, 
+                        num_intervals, overlap,
+                        *args):
+    ctx = dash.callback_context
+    if (not ctx.triggered) or (not contents):
+        return dash.no_update
+    
+    elements = []
 
+    df = contents_to_dataframe(contents)
+    filt = get_filter(filter_name, *args)
+    f_x = filt(df.values)
+    cover = IntervalCover.EvenlySpacedFromValues(f_x, int(num_intervals), float(overlap) / 100)
+    print(cover)
+    return elements
